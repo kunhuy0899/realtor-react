ae79a7bb7cc859b715d264d69e3ac3cbab800216
kunhuy0899 <55186853+kunhuy0899@users.noreply.github.com>
2023-02-21 15:38:07 +0700

    create the header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import {Route, BrowserRouter as Router, Routes}  from "react-router-dom"
 
 import ForgotPassword from "./pages/ForgotPassword";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
-function App() {
+function App() { 
   return (
    <>
+    
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/offers" element={<Offers/>} />
         <Route path="/profile" element={<Profile/>} />
-        <Route path="/signin" element={<SignIn/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/forgotpass" element={<ForgotPassword/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/forgot-pass" element={<ForgotPassword/>} />
       </Routes>
     </Router>
    </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div >
-        <h1 className='text-2xl bg-red-400'>Home Hello</h1>
+        <h1>Home Hello tui</h1>
     </div>
   )
 }
