3ae72e2ec0ee91d0156fe6b14bad2911b17eed77
kunhuy0899 <55186853+kunhuy0899@users.noreply.github.com>
2023-02-21 13:31:57 +0700

    create pages and commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import {Route, BrowserRouter as Router, Routes}  from "react-router-dom"
+
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
    <>
-    <h1 className='text-2xl bg-red-400'>Hello Word</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/offers" element={<Offers/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/forgotpass" element={<ForgotPassword/>} />
+      </Routes>
+    </Router>
    </>
   );
 }
