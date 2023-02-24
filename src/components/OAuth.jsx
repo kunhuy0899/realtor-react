40cd37b4e723918a56160d33f38478c42a0c7d7e
kunhuy0899 <55186853+kunhuy0899@users.noreply.github.com>
2023-02-24 12:02:21 +0700

    compelete the oauth function ality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,40 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth"
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore"
+
 import {FcGoogle} from "react-icons/fc"
 import React from 'react'
+import { db } from "../firebase"
+import { toast } from "react-toastify"
+import { useNavigate } from "react-router-dom"
 
 export default function OAuth() {
+  const naviGate=useNavigate();
+  async function onGoogleClick() {
+      try{
+        const auth = getAuth();
+        const provider= new GoogleAuthProvider();
+        const result = await signInWithPopup(auth,provider);
+        const user =result.user;
+        //check for the user
+        const docRef = doc(db,"users",user.uid)
+        const docSnap=await getDoc(docRef);
+        if(!docSnap.exists()){
+          await setDoc(docRef,{
+            name:user.displayName,
+            email:user.email,
+            timestamp:serverTimestamp(),
+          });
+        }
+        naviGate("/")
+
+      } catch (error){
+        toast.error("Could not authorize with Google !!!!")
+      }
+  }
   return (
-    <button 
+    <button
+        type="button"
+        onClick={onGoogleClick} 
         className="flex items-center 
                     justify-center w-full 
                     bg-red-700 text-white 
