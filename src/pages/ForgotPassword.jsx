5aee45cc75e50faa12ab17267ba0c993c78bc30a
kunhuy0899 <55186853+kunhuy0899@users.noreply.github.com>
2023-02-24 16:58:40 +0700

    copelete password page function

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { toast } from 'react-toastify';
 
 export default function ForgotPassword() {
   const [email,setEmail]=useState("");
   function onChange(e){
     setEmail(e.target.value)
   }
+  async function onSubmit(e){
+      e.preventDefault();
+      try {
+        const auth = getAuth();
+        await sendPasswordResetEmail(auth,email)
+        toast.success("Email was sent");
+
+      } catch (error){
+        toast.error("could not send reset password");
+      }
+  } 
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Forgot Password</h1>
@@ -16,7 +29,7 @@ export default function ForgotPassword() {
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80" alt="key" className='w-full rounded-2xl ' />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <input 
               type="email"
               id='email'
